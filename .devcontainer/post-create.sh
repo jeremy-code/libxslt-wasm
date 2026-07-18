3363f16e001a27ef2d391e9d358ec8d6e879b785
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-07-18 04:12:52 +0000

    chore: fix pnpm store path

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 corepack enable
-# Otherwise, `pnpm store path` returns `/workspaces/libxslt-wasm/.pnpm-store/v10`
-pnpm config set store-dir "${XDG_DATA_HOME:="$HOME/.local/share"}/pnpm/store" --global
 pnpm install
 pnpm compile
 pnpm build

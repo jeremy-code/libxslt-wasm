7709d56f06116f85951ba9f5fbdf72dff2bc6d1d
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-07-18 21:04:25 +0000

    chore: remove extraneous runtime_methods

---
+++ .changeset/silver-squids-smash.md
@@ -0,0 +1,5 @@
+---
+"libxslt-wasm": patch
+---
+
+chore: remove extraneous runtime_methods
--- src/internal/emscripten.ts
+++ src/internal/emscripten.ts
@@ -3,34 +3,15 @@ import { libxslt } from "./module.ts";
 export const {
   // preamble.js
   HEAPU8,
-  HEAP16,
-  HEAPU16,
-  HEAP32,
-  HEAPU32,
-  HEAPF32,
-  HEAPF64,
-  addRunDependency,
-  removeRunDependency,
 
   // libcore.js
   ptrToString,
-  warnOnce,
-  handleException,
   getNativeTypeSize,
-
-  // libccall.js
-  ccall,
-  cwrap,
 
   // libstrings.js
   UTF8ToString,
-  stringToUTF8Array,
-  stringToUTF8,
   lengthBytesUTF8,
-  intArrayFromString,
-  intArrayToString,
   stringToNewUTF8,
-  writeArrayToMemory,
 
   // libgetvalue.js
   getValue,

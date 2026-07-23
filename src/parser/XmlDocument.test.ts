cc7c10c49f7db18af7579ab65c4f569f0cb01fc3
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-07-23 14:44:33 -0700

    test: add tests for XmlDocument .toString, .toXmlOutputBuffer

--- src/parser/XmlDocument.test.ts
+++ src/parser/XmlDocument.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, test } from "vitest";
 
 import { XmlDocument } from "./XmlDocument.ts";
 import { NULL_POINTER } from "../constants.ts";
+import { lengthBytesUTF8 } from "../internal/emscripten.ts";
 
 const TEST_XML_STRING = `<?xml version="1.0" encoding="UTF-8"?>
 <root/>
@@ -43,6 +44,25 @@ describe("XmlDocument", () => {
         options: { pedantic: true },
       });
       expect(xmlDocument.byteOffset).not.toBe(NULL_POINTER);
+      expect(xmlDocument.toString()).toBe(TEST_XML_STRING);
+      xmlDocument.delete();
+    });
+  });
+
+  describe(".toXmlOutputBuffer()", () => {
+    test("should create a XML Output Buffer", async () => {
+      const xmlDocument = await XmlDocument.fromString(TEST_XML_STRING);
+      const xmlOutputBuffer = xmlDocument.toXmlOutputBuffer();
+      expect(xmlOutputBuffer.byteOffset).not.toBe(NULL_POINTER);
+      expect(xmlOutputBuffer.size).toBe(lengthBytesUTF8(TEST_XML_STRING));
+      xmlOutputBuffer.delete();
+      xmlDocument.delete();
+    });
+  });
+
+  describe(".toString()", () => {
+    test("should create string output", async () => {
+      const xmlDocument = await XmlDocument.fromString(TEST_XML_STRING);
       expect(xmlDocument.toString()).toBe(TEST_XML_STRING);
       xmlDocument.delete();
     });
